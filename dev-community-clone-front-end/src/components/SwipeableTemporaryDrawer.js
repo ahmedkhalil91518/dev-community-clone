1e0c3f292dbdd0174f70b9e1571df527c8862bd4
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-06-19 00:15:16 +0200

    add a cancel button to the sidenav

--- dev-community-clone-front-end/src/components/SwipeableTemporaryDrawer.js
+++ dev-community-clone-front-end/src/components/SwipeableTemporaryDrawer.js
@@ -1,15 +1,17 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import MenuIcon from "@mui/icons-material/Menu";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+
 export default function SwipeableTemporaryDrawer() {
   const [state, setState] = React.useState({
     left: false,
@@ -18,8 +20,8 @@ export default function SwipeableTemporaryDrawer() {
   const toggleDrawer = (anchor, open) => (event) => {
     if (
       event &&
-      event.type === 'keydown' &&
-      (event.key === 'Tab' || event.key === 'Shift')
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
     ) {
       return;
     }
@@ -33,8 +35,10 @@ export default function SwipeableTemporaryDrawer() {
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
-      <List className='sidenav'>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+      <List className="sidenav">
+        <h3 className="h3-header">DEV Community</h3>
+        <HighlightOffIcon className="cancel-icon" onClick={toggleDrawer(anchor, false)}/>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
@@ -51,9 +55,12 @@ export default function SwipeableTemporaryDrawer() {
 
   return (
     <div>
-      {['left'].map((anchor) => (
+      {["left"].map((anchor) => (
         <React.Fragment key={anchor}>
-          <MenuIcon onClick={toggleDrawer(anchor, true)} className="burger-button" />
+          <MenuIcon
+            onClick={toggleDrawer(anchor, true)}
+            className="burger-button"
+          />
           <SwipeableDrawer
             anchor={anchor}
             open={state[anchor]}
