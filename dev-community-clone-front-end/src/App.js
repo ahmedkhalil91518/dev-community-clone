447ba0f454f3a48dc9c92c993466c205fac6a99f
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-06-18 17:51:40 +0200

    add SwipeableTemporaryDrawer

--- dev-community-clone-front-end/src/App.js
+++ dev-community-clone-front-end/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import SwipeableTemporaryDrawer from "./components/SwipeableTemporaryDrawer";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <SwipeableTemporaryDrawer />
+    </div>
+  );
 }
 
 export default App;
