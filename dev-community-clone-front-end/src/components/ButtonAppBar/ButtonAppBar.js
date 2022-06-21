26a504e0f86532f1ee303430a286e2b45e59871e
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-06-21 08:58:01 +0200

    adjust the style of ButtonAppBar component

--- dev-community-clone-front-end/src/components/ButtonAppBar/ButtonAppBar.js
+++ dev-community-clone-front-end/src/components/ButtonAppBar/ButtonAppBar.js
@@ -1,36 +1,29 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import SwipeableTemporaryDrawer from "../SwipeableTemporaryDrawer";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
 import CreateAccountButton from "../CreateAccountButton";
-import ButtonAppBarCSS from "./ButtonAppBar.module.css"
+import ButtonAppBarCSS from "./ButtonAppBar.module.css";
 
 export default function ButtonAppBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar className={ButtonAppBarCSS.appbar}>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <SwipeableTemporaryDrawer />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button className={ButtonAppBarCSS.searchButton}><SearchIcon/></Button>
-          <CreateAccountButton />
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="sticky">
+      <Toolbar className={ButtonAppBarCSS.appbar}>
+        <IconButton>
+          <SwipeableTemporaryDrawer />
+        </IconButton>
+        <h1 className={ButtonAppBarCSS.appTitle}>
+          News
+        </h1>
+        <Button className={ButtonAppBarCSS.searchButton}>
+          <SearchIcon />
+        </Button>
+        <CreateAccountButton />
+      </Toolbar>
+    </AppBar>
   );
 }
