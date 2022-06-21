469c9f3b981b28355e9b80b314c03f5f9bcc96b1
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-06-21 12:12:02 +0200

    add social login buttons

--- dev-community-clone-front-end/src/components/LoginPage/LoginPage.js
+++ dev-community-clone-front-end/src/components/LoginPage/LoginPage.js
@@ -1,12 +1,41 @@
-import React from 'react'
-import LoginPageCSS from "./LoginPage.module.css"
-import GithubButton from "../ReactSocialLoginButtons/GithubLoginButton"
-import GoogleButton from"../ReactSocialLoginButtons/GoogleLoginButton"
+import React from "react";
+import LoginPageCSS from "./LoginPage.module.css";
+import GithubButton from "../ReactSocialLoginButtons/GithubLoginButton";
+import GoogleButton from "../ReactSocialLoginButtons/GoogleLoginButton";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import createButton from '../ReactSocialLoginButtons/create-button';
+import createSvgIcon from '../ReactSocialLoginButtons/createSvgIcon';
+import EmailIcon from '@mui/icons-material/Email';
+function Icon({ width, height, color }) {
+  return (<EmailIcon />)
+}
 
+const config = {
+  activeStyle: { background: '#EFF0EE' },
+  icon: createSvgIcon(Icon),
+  style: { background: 'white', color: 'black' },
+  text: 'Log in with Email',
+};
+
+const EmailLoginButton = createButton(config);
 function LoginPage() {
   return (
     <div className={LoginPageCSS.page}>
-    </div> )
+      <Card>
+        <CardContent>
+          <Typography variant="h6" component="div">
+            DEV Community is a community of 863,727 amazing developers
+          </Typography>
+        </CardContent>
+        <CardActions><GoogleButton/></CardActions>
+        <CardActions><GithubButton /></CardActions>
+        <CardActions><EmailLoginButton /></CardActions>
+      </Card>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
