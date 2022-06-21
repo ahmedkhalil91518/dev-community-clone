c7f1a25f2d35f1c0c5d2221c916efed7adefcf52
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-06-21 11:23:36 +0200

    add ReactSocialLoginButtons

--- dev-community-clone-front-end/src/components/LoginPage/LoginPage.js
+++ dev-community-clone-front-end/src/components/LoginPage/LoginPage.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import LoginPageCSS from "./LoginPage.module.css"
-
+import GithubButton from "../ReactSocialLoginButtons/GithubLoginButton"
+import GoogleButton from"../ReactSocialLoginButtons/GoogleLoginButton"
 
 function LoginPage() {
   return (
     <div className={LoginPageCSS.page}>
-      
     </div> )
 }
 
