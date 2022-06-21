156b5a39c2e60187f8f01d4d3539398ca0a5ad51
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-06-21 08:25:52 +0200

    change Main.js to use css modules

--- dev-community-clone-front-end/src/App.js
+++ dev-community-clone-front-end/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import ButtonAppBar from "./components/ButtonAppBar";
-import Main from "./components/Main";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
---
+++ dev-community-clone-front-end/src/components/Main/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import MainCSS from "./Main.module.css"
+function Main() {
+  return (
+    <main className={MainCSS.main}></main>
+  )
+}
+
+export default Main
