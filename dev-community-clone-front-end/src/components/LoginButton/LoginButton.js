e2f6882f9ced44c5b6bfc5f6e1c58e061751d577
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-06-23 16:27:53 +0200

    finish the divider container part

--- dev-community-clone-front-end/src/components/LoginButton/LoginButton.js
+++ dev-community-clone-front-end/src/components/LoginButton/LoginButton.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from "@mui/material/Button";
 import LoginButtonCSS from "./LoginButton.module.css";
 import {  Link } from"react-router-dom";
 
--- dev-community-clone-front-end/src/components/LoginPage/LoginPage.js
+++ dev-community-clone-front-end/src/components/LoginPage/LoginPage.js
@@ -6,18 +6,20 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import createButton from '../ReactSocialLoginButtons/create-button';
-import createSvgIcon from '../ReactSocialLoginButtons/createSvgIcon';
-import EmailIcon from '@mui/icons-material/Email';
+import createButton from "../ReactSocialLoginButtons/create-button";
+import createSvgIcon from "../ReactSocialLoginButtons/createSvgIcon";
+import EmailIcon from "@mui/icons-material/Email";
+import Divider from "@mui/material/Divider";
+
 function Icon({ width, height, color }) {
-  return (<EmailIcon />)
+  return <EmailIcon />;
 }
 
 const config = {
-  activeStyle: { background: '#EFF0EE' },
+  activeStyle: { background: "#EFF0EE" },
   icon: createSvgIcon(Icon),
-  style: { background: 'white', color: 'black' },
-  text: 'Log in with Email',
+  style: { background: "white", color: "black" },
+  text: "Log in with Email",
 };
 
 const EmailLoginButton = createButton(config);
@@ -30,9 +32,19 @@ function LoginPage() {
             DEV Community is a community of 863,727 amazing developers
           </Typography>
         </CardContent>
-        <CardActions><GoogleButton/></CardActions>
-        <CardActions><GithubButton /></CardActions>
-        <CardActions><EmailLoginButton /></CardActions>
+        <CardActions>
+          <GoogleButton />
+        </CardActions>
+        <CardActions>
+          <GithubButton />
+        </CardActions>
+        <CardActions>
+          <EmailLoginButton />
+        </CardActions>
+        <CardContent className={LoginPageCSS.dividerContainer}>
+          <Divider className={LoginPageCSS.divider}/>
+          <div className={LoginPageCSS.dividerText}>Have a password? Continue with your email address</div>
+        </CardContent>
       </Card>
     </div>
   );
