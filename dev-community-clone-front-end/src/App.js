3b2ff8d9864fe3562b485e6e8be04736f148a794
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-06-24 08:17:17 +0200

    adjust the styling of the login page

--- dev-community-clone-front-end/src/App.js
+++ dev-community-clone-front-end/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import ButtonAppBar from "./components/ButtonAppBar/ButtonAppBar";
 import Main from "./components/Main/Main";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import LoginPage from "./components/LoginPage/LoginPage";
 import BasicCard from "./components/BasicCard/BasicCard";
@@ -13,7 +13,7 @@ function App() {
         <Main />
         <Routes>
           <Route path="/" element={<BasicCard />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />}/>
         </Routes>
       </Router>
     </>
