f5e225829dbf4b3f2e0add4827436a5de44f607a
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-06-27 01:44:19 +0200

    connect mongoose to mongodb

--- dev-community-clone-back-end/app.js
+++ dev-community-clone-back-end/app.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const mongoose = require('mongoose');
+require('dotenv').config()
 const app = express();
 const port = 5000;
  
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+main().catch((err) => console.log(err));
+ 
+async function main() {
+ await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+//  const userSchema = new mongoose.Schema({
+//    firstName: String,
+//    lastName: String,
+//    email: String,
+//    password: String,
+//    role: String,
+//    summary: String
+//  });
+//  const User = mongoose.model('User', userSchema);
+//   app.post('/',  async (req, res) => {
+//    const user = new User({
+//      firstName: 'String',
+//      lastName: 'String',
+//      email: 'String',
+//      password: 'String',
+//      role: 'String',
+//      summary: 'String',
+//    });
+//     await user.save();
+//     res.send(user);
+//  });
+}
+
  
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
