20e06ba0cec55b6b201a7ca61ac40af6b97fe6b0
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-06-27 06:55:42 +0200

    add some useful middlewares

--- dev-community-clone-back-end/app.js
+++ dev-community-clone-back-end/app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const unknownEndpoint = require("./middlewares/unknownEndpoint");
+const errorHandler = require("./middlewares/errorHandler");
+
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
+app.use(cors());
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING)
@@ -19,6 +23,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/signup", signupRouter);
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
