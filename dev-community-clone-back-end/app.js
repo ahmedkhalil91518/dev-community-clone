95376c2dce1a49b134783eef1555b82b10404ef6
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-06-27 06:57:37 +0200

    add missing signup require

--- dev-community-clone-back-end/app.js
+++ dev-community-clone-back-end/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const signupRouter = require("./controllers/signupRouter")
 const unknownEndpoint = require("./middlewares/unknownEndpoint");
 const errorHandler = require("./middlewares/errorHandler");
 
