8d599664bec8ecaed7edb976008cd458d5a2c9e7
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-06-29 00:14:09 +0200

    add the content of the about page

--- dev-community-clone-front-end/src/components/About/About.js
+++ dev-community-clone-front-end/src/components/About/About.js
@@ -3,7 +3,12 @@ import AboutCSS from "./About.module.css"
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className={AboutCSS.page}><h1>About DEV</h1>
+    DEV is a community of software developers getting together to help one another out. The software industry relies on collaboration and networked learning. We provide a place for that to happen.
+    
+    DEV is built on Forem: open source software designed to empower communities. Because our application is open source, you can inspect every little detail of the code, or chip in yourself! Forem is available for anyone interested in creating similar communities in any niche or passion. Visit our meta Forem, forem.dev for more information.
+    
+    We believe in transparency and adding value to the ecosystem. We hope you enjoy poking around and participating!</div>
   )
 }
 
