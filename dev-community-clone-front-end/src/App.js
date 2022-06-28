3b7253010b32c81a8817d7aae0bb440e7e967883
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-06-28 05:02:37 +0200

    enhance the structure of the react router

--- dev-community-clone-front-end/src/App.js
+++ dev-community-clone-front-end/src/App.js
@@ -1,26 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
-import MainBar from './components/MainBar/MainBar';
+import MainBar from "./components/MainBar/MainBar";
 import Main from "./components/Main/Main";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Footer from './components/Footer/Footer';
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 import LoginPage from "./components/LoginPage/LoginPage";
-import Posts from './components/Posts/Posts';
-import SignupPage from './components/SignupPage/SignupPage';
+import Posts from "./components/Posts/Posts";
+import SignupPage from "./components/SignupPage/SignupPage";
 
 function App() {
   return (
     <>
-      <Router>
-        <MainBar />
-        <Main />
-        <Routes>
-          <Route path="/" element={<Posts />} />
-          <Route path="/login" element={<LoginPage />}/>
-          <Route path="/signup" element={<SignupPage />}/>
-        </Routes>
-        <Footer />
-      </Router>
+      <MainBar />
+      <Main />
+      <Routes>
+        <Route path="/" element={<Posts />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
