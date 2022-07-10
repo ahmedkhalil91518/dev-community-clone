af81ad32d7e890bad858b44bbfa9bf600182f59d
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-07-11 01:42:52 +0200

    fixed a bug where the user entarctor unable to get the user name

--- dev-community-clone-back-end/middlewares/userExtractor.js
+++ dev-community-clone-back-end/middlewares/userExtractor.js
@@ -5,15 +5,15 @@ const userExtractor = async (request, response, next) => {
   
   try {
     const decodedToken = jwt.verify(request.token, process.env.SECRET);
-    if (!request.token || !decodedToken.email) {
+    if (!request.token || !decodedToken.id) {
       console.log(request.token);
       console.log(decodedToken);
       return response.status(401).json({ error: "token missing or invalid" });
     }
     console.log("user2");
-    const user = await User.findOne({email:decodedToken.email})
+    const user = await User.findOne({_id:decodedToken.id})
     request.user = user;
-    
+    console.log(decodedToken);
     next();
   } catch (error) {
     next(error);
