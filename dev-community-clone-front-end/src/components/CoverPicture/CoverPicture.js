bf2a904dfd7250aef0af79e5d85726b343ebe28f
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-07-10 04:15:49 +0200

    add cover image upload to firebase storage

--- dev-community-clone-front-end/src/components/CoverPicture/CoverPicture.js
+++ dev-community-clone-front-end/src/components/CoverPicture/CoverPicture.js
@@ -3,23 +3,61 @@ import ImageUploading from "react-images-uploading";
 import CoverPictureCSS from "./CoverPicture.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { coverPicture } from "reducers/newPostReducer";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { app } from "../../firebase";
 
 export function CoverPicture() {
   const [image, setImage] = React.useState([]);
+  const [progress, setProgress] = React.useState(0);
   const dispatch = useDispatch();
   const maxNumber = 1;
-// @ts-ignore
-const prisitedImage = useSelector(state => state.newPost)
+  // @ts-ignore
+  const persistedImage = useSelector((state) => state.newPost);
+  const user = useSelector((state) => {
+    // @ts-ignore
+    return state.auth;
+  });
+
+  // @ts-ignore
+  const auth = useSelector((state) => state.auth);
   const onChange = (uploadImage) => {
     // data for submit
-    console.log(uploadImage);
+    console.log(user);
     setImage(uploadImage);
-    dispatch(coverPicture(JSON.stringify(uploadImage)))
+    dispatch(coverPicture(JSON.stringify(uploadImage)));
     console.log(uploadImage);
+    upload(uploadImage[0].file)
+  };
+  const configuredStorage = getStorage(app);
+  const upload = (file) => {
+    if (!file) return;
+    const storageRef = ref(
+      configuredStorage,
+      `/files/covers/${auth.uid}/${file.name}`
+    );
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const prog = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(prog);
+      },
+      (err) => {
+        console.log(err);
+      }, () => {
+        const durl = getDownloadURL(uploadTask.snapshot.ref).then(url => {
+          console.log(url);
+        })
+        
+      }
+    );
   };
 
   return (
     <div>
+      <h1>uploaded {progress} %</h1>
       <ImageUploading
         value={image}
         onChange={onChange}
@@ -44,7 +82,7 @@ const prisitedImage = useSelector(state => state.newPost)
                 onClick={onImageUpload}
                 {...dragProps}
                 className={CoverPictureCSS.button}
-                disabled={image[0] ? true: false}
+                disabled={image[0] ? true : false}
               >
                 Click or Drop here your cover photo
               </button>
@@ -52,10 +90,24 @@ const prisitedImage = useSelector(state => state.newPost)
             </div>
             {imageList.map((image, index) => (
               <div key={index} className={CoverPictureCSS.imageItem}>
-                <img src={image["data_url"]} alt=""  className={CoverPictureCSS.image}/>
+                <img
+                  src={image["data_url"]}
+                  alt=""
+                  className={CoverPictureCSS.image}
+                />
                 <div className={CoverPictureCSS.imageItemButtons}>
-                  <button onClick={() => onImageUpdate(index)} className={CoverPictureCSS.button}>Update</button>
-                  <button onClick={() => onImageRemove(index)} className={CoverPictureCSS.button}>Remove</button>
+                  <button
+                    onClick={() => onImageUpdate(index)}
+                    className={CoverPictureCSS.button}
+                  >
+                    Update
+                  </button>
+                  <button
+                    onClick={() => onImageRemove(index)}
+                    className={CoverPictureCSS.button}
+                  >
+                    Remove
+                  </button>
                 </div>
               </div>
             ))}
