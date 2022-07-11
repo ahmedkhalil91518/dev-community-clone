aed4e061d15e4a25da4aebe9284a0b860243ad63
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-07-11 09:52:01 +0200

    create the backend infinit scroll feature

--- dev-community-clone-back-end/controllers/viewPostsRouter.js
+++ dev-community-clone-back-end/controllers/viewPostsRouter.js
@@ -3,8 +3,38 @@ const Tag = require("../models/tag");
 const viewPostsRouter = require("express").Router();
 
 viewPostsRouter.get("/", async (request, response, next) => {
-  const posts = await Post.find({}).populate("author").populate("tags");
-  response.send(posts);
+  const page = parseInt(request.query.page);
+  const limit = parseInt(request.query.limit);
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  let results = {};
+
+  if (endIndex < (await Post.countDocuments().exec())) {
+    results.next = {
+      page: page + 1,
+      limit: limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    results.previous = {
+      page: page - 1,
+      limit: limit,
+    };
+  }
+  try {
+    results = await Post.find()
+      .populate("author")
+      .populate("tags")
+      .limit(limit)
+      .skip(startIndex)
+      .exec();
+    response.json(results);
+  } catch (e) {
+    response.status(500).json({ message: e.message });
+  }
 });
 
 viewPostsRouter.get("/post/:id", async (request, response, next) => {
@@ -15,9 +45,35 @@ viewPostsRouter.get("/post/:id", async (request, response, next) => {
 });
 
 viewPostsRouter.get("/tag/:tag", async (request, response, next) => {
-  const tag = await Tag.findOne({value: request.params.tag})
+  const page = parseInt(request.query.page);
+  const limit = parseInt(request.query.limit);
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  let results = {};
+
+  if (endIndex < (await Post.countDocuments().exec())) {
+    results.next = {
+      page: page + 1,
+      limit: limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    results.previous = {
+      page: page - 1,
+      limit: limit,
+    };
+  }
+  const tag = await Tag.findOne({ value: request.params.tag });
   console.log(tag);
-  const posts = await Post.find({tags: { $elemMatch: { $eq: tag._id } }}).populate("author").populate("tags");
+  const posts = await Post.find({ tags: { $elemMatch: { $eq: tag._id } } })
+    .populate("author")
+    .populate("tags")
+    .limit(limit)
+    .skip(startIndex)
+    .exec();
   response.send(posts);
 });
 
