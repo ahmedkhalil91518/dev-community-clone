67d6432165c56a39727c02aa11e11d323ed8c899
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-07-25 17:47:44 +0200

    fixed a problem where the posts style not showing up

--- dev-community-clone-front-end/src/components/SinglePost/SinglePost.js
+++ dev-community-clone-front-end/src/components/SinglePost/SinglePost.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState ,useCallback} from "react";
 import SinglePostCSS from "./SinglePost.module.css";
 import { useParams, Link } from "react-router-dom";
 import { showSinglePost } from "services/viewPostsService";
@@ -8,13 +8,81 @@ import { createEditor } from "slate";
 export const SinglePost = () => {
   const [post, setPost] = useState(null);
   const params = useParams();
+  const Element = ({ attributes, children, element }) => {
+    const style = { textAlign: element.align };
+    switch (element.type) {
+      case "block-quote":
+        return (
+          <blockquote style={style} {...attributes}>
+            {children}
+          </blockquote>
+        );
+      case "bulleted-list":
+        return (
+          <ul style={style} {...attributes}>
+            {children}
+          </ul>
+        );
+      case "heading-one":
+        return (
+          <h1 style={style} {...attributes}>
+            {children}
+          </h1>
+        );
+      case "heading-two":
+        return (
+          <h2 style={style} {...attributes}>
+            {children}
+          </h2>
+        );
+      case "list-item":
+        return (
+          <li style={style} {...attributes}>
+            {children}
+          </li>
+        );
+      case "numbered-list":
+        return (
+          <ol style={style} {...attributes}>
+            {children}
+          </ol>
+        );
+      default:
+        return (
+          <p style={style} {...attributes}>
+            {children}
+          </p>
+        );
+    }
+  };
+  
+  const Leaf = ({ attributes, children, leaf }) => {
+    if (leaf.bold) {
+      children = <strong>{children}</strong>;
+    }
+  
+    if (leaf.code) {
+      children = <code>{children}</code>;
+    }
+  
+    if (leaf.italic) {
+      children = <em>{children}</em>;
+    }
+  
+    if (leaf.underline) {
+      children = <u>{children}</u>;
+    }
+  
+    return <span {...attributes}>{children}</span>;
+  };
   // @ts-ignore
   const [editor] = useState(withReact(createEditor()));
   const id = params.id;
-
+  const renderElement = useCallback((props) => <Element {...props} />, []);
+  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
   useEffect(() => {
     showSinglePost(id).then((fetchedPost) => {
-      setPost(fetchedPost.data);
+      setPost({...fetchedPost.data , content:JSON.parse(fetchedPost.data.content)});
       console.log(fetchedPost);
     });
   }, []);
@@ -52,9 +120,10 @@ export const SinglePost = () => {
               );
             })}
           </div>
-          {JSON.parse(post.content) && (
-            <Slate editor={editor} value={JSON.parse(post.content)}>
-              <Editable readOnly />
+          {post.content && (
+            <Slate editor={editor} value={post.content}>
+              <Editable readOnly renderElement={renderElement}
+                  renderLeaf={renderLeaf}/>
             </Slate>
           )}
         </div>
